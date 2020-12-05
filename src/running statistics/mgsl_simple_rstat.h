d5fa5bce60c5a2f956733daf50385d74dd8f8c4b
joscao <62660664+joscao@users.noreply.github.com>
2020-12-05 17:29:37 +0100

    license disclaimer

--- src/running statistics/mgsl_simple_rstat.h
+++ src/running statistics/mgsl_simple_rstat.h
@@ -1,3 +1,19 @@
+// Copyright (C) [2020] [Jonathan Schmalfuß]
+//
+// This file is part of the modern Generic Scientific Library (mGSL).
+// mGSL is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// mGSL is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License 
+// along with mGSL; if not, see <https://www.gnu.org/licenses>.
+
 #pragma once
 
 #include <cstddef>
